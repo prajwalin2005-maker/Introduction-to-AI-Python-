db2248ab7adaeef2a8a60befb04bc8e58dac0184
prajwalin2005-maker <prajwal.in.2005@gmail.com>
2026-08-10 22:02:46 +0530

    Create data_types.py

---
+++ Python_Basics/data_types.py
@@ -0,0 +1,52 @@
+# Python Data Types
+
+# Integer
+age = 21
+
+# Float
+height = 5.8
+
+# String
+name = "Prajwal"
+
+# Boolean
+is_student = True
+
+# List
+subjects = ["AI", "Python", "Machine Learning"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Set
+numbers = {1, 2, 3, 4}
+
+# Dictionary
+student = {
+    "name": "Prajwal",
+    "age": 21,
+    "domain": "AI & ML"
+}
+
+# None
+result = None
+
+print("Integer:", age)
+print("Float:", height)
+print("String:", name)
+print("Boolean:", is_student)
+print("List:", subjects)
+print("Tuple:", coordinates)
+print("Set:", numbers)
+print("Dictionary:", student)
+print("None:", result)
+
+# Checking data types
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+print(type(subjects))
+print(type(coordinates))
+print(type(numbers))
+print(type(student))
