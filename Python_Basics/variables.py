f0fb1f3dc8e37ea50baadbc9fafaff3bdb3e3bea
prajwalin2005-maker <prajwal.in.2005@gmail.com>
2026-08-10 22:00:21 +0530

    Create variables.py

---
+++ Python_Basics/variables.py
@@ -0,0 +1,16 @@
+# Python Variables
+
+name = "Prajwal"
+age = 21
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Changing a variable
+age = 22
+
+print("Updated Age:", age)
